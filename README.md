dc563491e70c235c8b629b00d0154287349b4bf7
Filip Joelsson <filip@blueturtle.nu>
2014-11-17 15:05:19 +0100

    Commiting all local files

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 hack_upload
 ===========
 
-A Drupal 8 module for simple file upload. Useful for combination with REST based web apps
+What is this?
+A Drupal 8 module for simple file upload. 
+
+Why would I want it?
+It is currently useful for combining Drupal 8 with REST based web apps. 
+However, if Drupal 8 does get support for a more REST-like way to upload images
+(or other files), this module will be deprecated by that.
+
+And how does it work?
+The module does not change the db schema in any way. What it does, is setting 
+up a form for uploading a single file (jpg, gif or png - but that's easy to
+change). An unauthenticated user can get access to the form, but in the form
+logic, we abort unless the user has provided credentials via native login or at
+the very least HTTP Basic Auth (which is one of many hacks in the module).
+
+The file is saved as a temporary file, but in a public path. (Don't know if
+this was necessary.) The return value is a JSON object with the uuid and uri of
+the file. These can be used when POSTing och PATCHing a node via Drupal 8's 
+REST interface. When this is done, the file is going to gain permanent status.
+
+What's left?
+Setting up cron to clean out temporary files older than X minutes.
+Setting up a config for the path in which to save the files.
+Setting up a config for the file formats to accept.
+- or -
+Just fix the REST based file upload already... :-)
---
+++ src/Form/HackUploadForm.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\hack_upload\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements the fool file upload form
+ */
+
+class HackUploadForm extends FormBase {
+
+  public function getFormId() {
+    return 'hack_upload_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['new_file'] = array(
+      '#type' => 'file',
+      '#title' => 'A file',
+    );
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Upload'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+
+  private function _grep_to_log($str, $data, $c_b, $c_a) {
+    if (preg_match_all(
+      "~((?:[^\n]*\n){0,$c_b})[^\n]*$str.[^\n]*\n((?:[^\n]*\n){0,$c_a})~",
+       $data, $arr)) {
+      error_log(print_r($arr[0],TRUE));
+    }
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    #$data = print_r($form_state, TRUE);
+    #$str = '\[input.protected';
+    #$this->_grep_to_log($str, $data, 3, 10);
+
+    $file = NULL;
+    $ret = file_save_upload('new_file', array(), 'public://field/proof/');
+    #$ret = file_save_upload('new_file', array('file_validate_extensions'=>array(0=>'jpeg jpg gif png')), 'public://field/proof/');
+    if (is_array($ret) && is_object($ret[0])) {
+      $file = $ret[0];
+    }
+    else {
+      error_log(print_r($ret, TRUE));
+      if (is_array($ret)) {
+        error_log('$ret is an array of length '.count($ret));
+      }
+      $uuid = 0;
+      $uri = '';
+      $error = 'No useful file retrieved. Bug in uploading script? (Tip: Check form key of the binary.)';
+      error_log($error); 
+      die('{ "uri" : "", "uuid" : "0", "error" : "'. $error .'"}');
+    }
+
+    $user = \Drupal::currentUser();
+    if (!$user->isAuthenticated()) {
+      if ($_SERVER['PHP_AUTH_USER']) {
+        $user = user_load_by_name($_SERVER['PHP_AUTH_USER']);
+      } else {
+        die('{"error" : "Login required", "uuid" : "0", "uri" : "" }');
+      }
+    }
+    $uid = $user->id();
+
+    $file->setOwnerId($uid);
+    $file->save();
+    $uuid = $file->uuid();
+    $uri = $file->destination;
+
+    $json = '{ "uri" : "'. $uri .'", "uuid" : "'. $uuid .'"}';
+    print $json;
+    exit;
+  }
+
+}
